2a5b24447271a074ae25e458fbf3e7d8325c4377
Chritzt <christoph.purkart04@gmail.com>
2023-11-28 09:28:16 +0100

    starting index.html

--- wishlist_management/app.js
+++ wishlist_management/app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var childrenRouter  = require('/routes/children');
+var childrenRouter  = require('./routes/children');
 
 var app = express();
 
